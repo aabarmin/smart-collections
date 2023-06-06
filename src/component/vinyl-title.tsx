ddffa430fa2ed73070b5f9e5d96e0d3b81d12c4f
Aleksandr Barmin <abarmin@outlook.com>
2023-06-06 15:45:43 +0100

    Now update of title works. At least should

--- src/component/vinyl-title.tsx
+++ src/component/vinyl-title.tsx
@@ -3,19 +3,34 @@ import ButtonEdit from "./button-edit";
 import { useCallback, useState } from "react";
 import ButtonSubmit from "./button-submit";
 import ButtonCancel from "./button-cancel";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 interface Props {
     title: string;
     onUpdate: (value: string) => void
 }
 
+type Inputs = {
+    value: string; 
+}
+
 export default function VinylTitle({
-    title
+    title, 
+    onUpdate
 }: Props) {
     const [editable, setEditable] = useState(false);
     const toggleEdit = useCallback(() => {
         setEditable(!editable);
     }, [editable]);
+    const {register, handleSubmit} = useForm<Inputs>({
+        defaultValues: {
+            value: title
+        }
+    }); 
+    const onSubmit: SubmitHandler<Inputs> = (values) => {
+        onUpdate(values.value);
+        toggleEdit();
+    }
 
     if (!editable) {
         return (
@@ -31,16 +46,18 @@ export default function VinylTitle({
     }
 
     return (
-        <Row>
-            <Col>
-                <Form.Control type="text" />
-            </Col>
-            <Col xs={1}>
-                <ButtonSubmit />
-            </Col>
-            <Col xs={2}>
-                <ButtonCancel onClick={toggleEdit} />
-            </Col>
-        </Row>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Row>
+                <Col>
+                    <Form.Control type="text" {...register("value", {required: true})} />
+                </Col>
+                <Col xs={1}>
+                    <ButtonSubmit />
+                </Col>
+                <Col xs={2}>
+                    <ButtonCancel onClick={toggleEdit} />
+                </Col>
+            </Row>
+        </form>
     )
 }
