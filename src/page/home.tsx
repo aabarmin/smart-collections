23fbd2ff3382b27d4d7164409775189ca05b3335
Aleksandr Barmin <abarmin@outlook.com>
2023-06-13 14:20:20 +0000

    Fancy main page

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,12 +1,23 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import { useCallback } from "react";
+import { Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import WideButton from "../component/button-wide";
+import Header from "../component/header";
 
 export default function Home() {
+    const navigate = useNavigate();
+    const onGoLibrary = useCallback(() => {
+        navigate("/library")
+    }, [navigate]);
+
     return (
-        <div>
-            <Button variant="primary" href="/library">
-                To the library
-            </Button>
-        </div>
-    );    
+        <>
+            <Header />
+            <Container style={{ paddingTop: "16px" }}>
+                <WideButton
+                    title="To the library"
+                    onClick={onGoLibrary} />
+            </Container>
+        </>
+    );
 }
