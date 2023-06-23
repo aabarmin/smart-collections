14d37d93dbe6a3622e0a23a26fc78df70c65d732
Aleksandr Barmin <abarmin@outlook.com>
2023-06-23 11:19:31 +0100

    Adding scripts to build a bundle

--- .github/workflows/deploy_dev/run.sh
+++ .github/workflows/deploy_dev/run.sh
@@ -15,14 +15,16 @@ function frontend_copy()
 {
     echo "Copy frontend to backend"
 
-    rm -rf ./backend/resources/css
-    rm -rf ./backend/resources/js
+    rm -rf ./backend/public/css
+    rm -rf ./backend/public/js
     rm -f ./backend/public/favicon.ico
 
-    cp -R ./frontend/build/static/css ./backend/resources/css
-    cp -R ./frontend/build/static/js ./backend/resources/js
+    cp -R ./frontend/build/static/css ./backend/public/css
+    cp -R ./frontend/build/static/js ./backend/public/js
     cp ./frontend/build/favicon.ico ./backend/public/favicon.ico
     cp ./frontend/build/manifest.json ./backend/public/manifest.json
+    cp ./frontend/build/logo192.png ./backend/public/logo192.png
+    cp ./frontend/build/logo512.png ./backend/public/logo512.png
 
     echo "Done"
 }
@@ -31,8 +33,8 @@ function frontend_integrate()
 {
     echo "Integrate frontend and backend"
 
-    JS_FILE=$(ls -1 ./backend/resources/js/ | grep "main" | grep ".js" | grep -v ".js.")
-    CSS_FILE=$(ls -1 ./backend/resources/css | grep "main" | grep ".css" | grep -v ".css.")
+    JS_FILE=$(ls -1 ./backend/public/js | grep "main" | grep ".js" | grep -v ".js.")
+    CSS_FILE=$(ls -1 ./backend/public/css | grep "main" | grep ".css" | grep -v ".css.")
 
     TEMPLATE_FILE=$(pwd)/backend/resources/views/welcome.blade.php
     python3 $(pwd)/.github/workflows/deploy_dev/replace.py $TEMPLATE_FILE "<--js-resource-->" "{{ asset('js/${JS_FILE}') }}"
@@ -41,6 +43,6 @@ function frontend_integrate()
     echo "Done"
 }
 
-# frontend_build
-# frontend_copy
+frontend_build
+frontend_copy
 frontend_integrate
--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -33,5 +33,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
         });
+
+        $this->routes(function () {
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        });
     }
 }
