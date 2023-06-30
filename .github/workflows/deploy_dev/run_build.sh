2ee1d01bc49b69135a9dbd640fc07e20740c92c9
Aleksandr Barmin <abarmin@outlook.com>
2023-06-30 14:16:45 +0100

    Update the deployment pipeline

--- .github/workflows/deploy_dev/run_build.sh
+++ .github/workflows/deploy_dev/run_build.sh
@@ -87,7 +87,7 @@ function backend_prepare_env()
 
     echo "LOG_CHANNEL=stack" >> .env
     echo "LOG_DEPRECATIONS_CHANNEL=null" >> .env
-    echo "LOG_LEVEL=debug"
+    echo "LOG_LEVEL=debug" >> .env
 
     echo "Done"
 }
