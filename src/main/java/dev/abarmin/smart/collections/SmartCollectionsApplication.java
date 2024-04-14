397f11030ef5e486d916946b08ef6d77f8ae9536
Aleksandr Barmin <abarmin@outlook.com>
2024-04-14 15:49:52 +0100

    Start a new Java version

---
+++ src/main/java/dev/abarmin/smart/collections/SmartCollectionsApplication.java
@@ -0,0 +1,13 @@
+package dev.abarmin.smart.collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCollectionsApplication.class, args);
+	}
+
+}
