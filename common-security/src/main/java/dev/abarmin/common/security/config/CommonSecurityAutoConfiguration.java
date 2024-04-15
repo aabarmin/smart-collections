f721822fb20a39edaec95f97d954ed59306a0ae3
Aleksandr Barmin <abarmin@outlook.com>
2024-04-15 14:07:07 +0100

    Fixed problem with login page

--- common-security/src/main/java/dev/abarmin/common/security/config/CommonSecurityAutoConfiguration.java
+++ common-security/src/main/java/dev/abarmin/common/security/config/CommonSecurityAutoConfiguration.java
@@ -1,21 +1,16 @@
 package dev.abarmin.common.security.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static dev.abarmin.common.security.controller.LoginController.LOGIN_ENDPOINT;
 
@@ -24,25 +19,10 @@ import static dev.abarmin.common.security.controller.LoginController.LOGIN_ENDPO
 @ComponentScan("dev.abarmin.common.security")
 public class CommonSecurityAutoConfiguration {
     @Bean
-    @Order(1)
-    public SecurityFilterChain staticResourcesFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .securityMatcher(AntPathRequestMatcher.antMatcher("/webjars/**"))
-                .authorizeHttpRequests(authorise -> authorise
-                        .anyRequest().permitAll())
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-                .csrf(csrf -> csrf.disable())
-//                .requestCache(cache -> cache.disable())
-//                .securityContext(context -> context.disable())
-//                .sessionManagement(session -> session.disable())
-                .build();
-    }
-
-    @Bean
-    @Order(1)
     public SecurityFilterChain basicSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authorise -> authorise
+                        .requestMatchers("/webjars/**").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(login -> login
